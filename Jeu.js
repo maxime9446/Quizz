64b1b83e61c32a202097b77a111ead5743c6bd28
matheo-2001 <collinamatheo2001@gmail.com>
2022-10-25 15:54:43 +0200

    test

---
+++ Jeu.js
@@ -0,0 +1,39 @@
+let nbQuestions = 3;
+console.log(nbQuestions);
+alert ("Bienvenue sur ce quiz!" + "\n " + nbQuestions + " questions seront posées");
+
+
+// Question 1
+let reponseUtilisateur = prompt (`question 1/3
+Quel est l'âge du capitaine?
+1. 42
+2. 101
+3. 18`);
+let bonneReponse = 1; 
+
+console.log (reponseUtilisateur);
+
+if (reponseUtiliser == bonneReponse) {
+        console.log("T'as gagné");
+}
+else {
+    console.l0g("Bruuuh t'es nul");
+}
+
+
+// Question 2
+let reponseUtilisateur = prompt (`question 1/3
+Quel est l'âge du capitaine?
+1. 42
+2. 101
+3. 18`);
+let bonneReponse = 1; 
+
+console.log (reponseUtilisateur);
+
+if (reponseUtiliser == bonneReponse) {
+        console.log("T'as gagné");
+}
+else {
+    console.l0g("Bruuuh t'es nul");
+}
